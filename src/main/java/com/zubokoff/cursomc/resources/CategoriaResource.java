b06a01437cb2f41c1ac1e228d997f34516142c9d
Zubokoff <alezubokoff@gmail.com>
2019-06-13 23:09:54 -0300

    finalizando categoria e adicionando banco h2

--- src/main/java/com/zubokoff/cursomc/resources/CategoriaResource.java
+++ src/main/java/com/zubokoff/cursomc/resources/CategoriaResource.java
@@ -1,15 +1,27 @@
 package com.zubokoff.cursomc.resources;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.zubokoff.cursomc.domain.Categoria;
 
 @RestController
 @RequestMapping("/categorias")
 public class CategoriaResource {
 
 	@GetMapping
-	public String listar() {
-		return "categoria listar";
+	public List<Categoria> listar() {
+		Categoria cat1 = new Categoria(1, "Informática");
+		Categoria cat2 = new Categoria(2, "Escritório");
+		
+		List<Categoria> lista = new ArrayList<>();
+		lista.add(cat1);
+		lista.add(cat2);
+		
+		return lista;
 	}
 }
