ed6d7744b4296f067429322c3ad155a3c0887691
Zubokoff <alezubokoff@gmail.com>
2019-06-23 23:29:27 -0300

    endpoint pedido

---
+++ src/main/java/com/zubokoff/cursomc/resources/PedidoResource.java
@@ -0,0 +1,25 @@
+package com.zubokoff.cursomc.resources;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.zubokoff.cursomc.domain.Pedido;
+import com.zubokoff.cursomc.services.PedidoService;
+
+@RestController
+@RequestMapping("/pedidos")
+public class PedidoResource {
+
+	@Autowired
+	private PedidoService service;
+	
+	@GetMapping("/{id}")
+	public ResponseEntity<?> listar(@PathVariable Integer id) {
+		Pedido pedido = service.find(id); 
+		return ResponseEntity.ok().body(pedido);
+	}
+}
