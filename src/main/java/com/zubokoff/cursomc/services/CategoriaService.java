fee8c156ec18e1ec6557a46c96defe53d5735687
zubokoff <alexandre.zubokoff@ntk.com.br>
2019-06-26 10:28:53 -0300

    find all paginado

--- src/main/java/com/zubokoff/cursomc/services/CategoriaService.java
+++ src/main/java/com/zubokoff/cursomc/services/CategoriaService.java
@@ -3,6 +3,9 @@ package com.zubokoff.cursomc.services;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
 import com.zubokoff.cursomc.domain.Categoria;
@@ -19,5 +22,10 @@ public class CategoriaService {
 		Optional<Categoria> categoria = repo.findById(id);
 		return categoria.orElseThrow(() -> new ObjectNotFoundException(
 				"Objeto não encontrado! Id: " + id + ", tipo: " + Categoria.class.getName()));
+	}
+	
+	public Page<Categoria> findPage(Integer page, Integer size, String direction, String orderBy) {
+		PageRequest pageRequest = PageRequest.of(page, size, Direction.valueOf(direction), orderBy);
+		return repo.findAll(pageRequest);
 	}
 }
